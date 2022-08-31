d2f9f345ed5f5fd902acec53952770500363fe7a
Saif Bechan <saifbechan@gmail.com>
2022-08-31 04:05:43 +0200

    chore: added GA snippet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import '@fontsource/jura';
 
 import { ChakraProvider } from '@chakra-ui/react';
 import Head from 'next/head';
+import Script from 'next/script';
 import theme from '../styles/theme';
 import type { AppProps } from 'next/app';
 
@@ -18,8 +19,27 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link href="/favicon.ico" rel="icon" sizes="64x64 32x32 24x24 16x16" type="image/x-icon" />
         <link href="/logo192.png" rel="apple-touch-icon" />
         <link href="/manifest.json" rel="manifest" />
+        <meta charSet="utf-8" />
+        <meta content="#141526" name="theme-color" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta
+          content="react, typescript, javascript, p5js, github, nextjs, machine learning, genetic algorithm"
+          name="keywords"
+        />
         <title>Genetic Algorithm V2</title>
       </Head>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Q372V016YB');
+            `,
+        }}
+        id="ga-datalayer"
+      />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Q372V016YB" />
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
